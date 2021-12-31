66591279d96044904c4e4322f717cc5a99e6c3e4
Anjali Pathak <54896331+anjalipathak13@users.noreply.github.com>
2021-12-31 14:10:26 +0530

    Time: 869 ms (25.62%), Space: 209.2 MB (38.92%) - LeetHub

---
+++ count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    void merge(vector<int>&count,vector<pair<int,int>> &v,int l,int mid,int r)
+    {
+        vector<pair<int,int>> temp(r-l+1);
+        int i=l;
+        int j=mid+1;
+        int k=0;
+        while(i<=mid && j<=r)
+        {
+            if(v[i].first<=v[j].first){
+                temp[k++]=v[j++];
+            }
+            else{
+                count[v[i].second]+=r-j+1;
+                temp[k++]=v[i++];
+                    
+                }
+        }
+        while(i<=mid)
+            temp[k++]=v[i++];
+        while(j<=r)
+            temp[k++]=v[j++];
+        for(int i=l;i<=r;i++){
+            v[i]=temp[i-l];
+        }
+        
+    }
+    void mergesort(vector<int>&count,vector<pair<int,int>> &v,int l,int r)
+    {
+        if(l<r)
+        {
+        int  mid=l+(r-l)/2;
+        mergesort(count,v,l,mid);
+        mergesort(count,v,mid+1,r);
+        merge(count,v,l,mid,r);
+        }
+    }
+    
+    vector<int> countSmaller(vector<int>& nums) {
+        int n=nums.size();
+        vector<pair<int,int>> v(n);
+        
+       for(int i=0;i<n;i++){
+            pair<int, int> p;
+            p.first = nums[i];
+            p.second = i;
+            v[i] = p;
+        }
+        
+        
+        
+        vector<int>count(n,0);
+        mergesort(count,v,0,n-1);
+        return count;
+    }
+};
