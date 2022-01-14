e0c532475a333e3b22c8ad57aa9dc920cc6d60a6
Anjali Pathak <54896331+anjalipathak13@users.noreply.github.com>
2022-01-14 21:16:30 +0530

    Time: 8 ms (79.85%), Space: 6.9 MB (83.76%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-         int n=s.length();
-        int i=0;//->window start
-        int j=0;//->window end
-        int maxC=0;//->max no of repeating elements 
-        int ans=-1;
-        unordered_map<char,int> mp;
+        unordered_map<char,int>m;
+       // int curr_len=0;
+        int maxcount=0,ans=-1;
+        int i=0;
+        int j=0;
+        int n=s.length();
         while(j<n)
         {
-            mp[s[j]]++;
-            maxC=max(maxC,mp[s[j]]);
-            
-            int curr_len=j-i+1;//Curr Len of the window
-            if(curr_len-maxC>k) //the curr window has more than k replacable items case
+            m[s[j]]++;
+            maxcount=max(maxcount,m[s[j]]);
+               int curr_len=j-i+1;
+            if(curr_len-maxcount>k)
             {
-                mp[s[i]]--;
-                i++;// shrinking the window
+                m[s[i]]--;
+                i++;
             }
-            curr_len=j-i+1;// just in case i is changed
+            curr_len=j-i+1;
             ans=max(ans,curr_len);
             j++;
         }
-        
         return ans;
     }
 };
