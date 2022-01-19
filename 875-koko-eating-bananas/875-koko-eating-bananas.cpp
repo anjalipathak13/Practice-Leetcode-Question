20f7842df9df378624ccaabed1d72d65b3abe3fb
Anjali Pathak <54896331+anjalipathak13@users.noreply.github.com>
2022-01-19 17:05:44 +0530

    Time: 44 ms (75.12%), Space: 18.7 MB (99.39%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+   /* bool can_eat(int mid,vector<int>& piles, int h)
+    {
+        int hours_till=0;
+        for(int i=0;i<piles.size();i++)
+        {
+            if(piles[i]<=mid)
+            {
+                hours_till++;
+            }else{
+                int a=piles[i];
+                while(!(a<=0))
+                     {
+                     a=a-mid;
+                    hours_till++;
+                      }
+                     }
+                }
+        
+       if(hours_till>h)
+        return false;
+        else
+        return true;
+    }*/
+    bool can_eat(vector<int>& piles, int k, int h){
+       int hours = 0; //total hours to eat all the bananas
+        for(int i = 0; i < piles.size(); i++){
+            hours += (piles[i] + k -1)/k;
+        }
+        if(hours > h){
+            return false;
+        }
+        else{
+            return true;
+        }
+        
+    }
+   /* int minEatingSpeed(vector<int>& piles, int h) {
+        int low=0;
+        int high=INT_MIN;
+     for(int i=0;i<piles.size();i++)
+     {
+        if(piles[i]<low)
+            low=piles[i];
+         if(piles[i]>high)
+             high=piles[i];
+         }
+        int ans;
+          while(low<=high)
+          {
+              
+             int mid=(low+high)/2;
+              if(can_eat(piles,mid,h))
+              {
+                  ans=mid;
+                  high=mid-1;
+              }
+              else{
+                  low=mid+1;
+                  
+              }
+              
+          }
+        return ans;
+        
+    }
+};*/
+int minEatingSpeed(vector<int>& piles, int h) {
+        int maxi = *max_element(piles.begin(), piles.end());
+        int s = 1, e = maxi, ans = 0;
+        int mid = s + (e-s)/2;
+        while(s<=e){
+            if(can_eat(piles, mid, h)){
+                ans = mid;
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
+};
