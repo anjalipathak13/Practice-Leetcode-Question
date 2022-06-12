9ca952900d7ac0d436b970063ce689ac65d4786d
Anjali Pathak <54896331+anjalipathak13@users.noreply.github.com>
2022-06-12 14:22:34 +0530

    Time: 375 ms (73.51%), Space: 99.6 MB (82.38%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+      /* unordered_map<int,int>m;
+        int l=0;
+        int r=0;
+        int sum=0;
+        int len=0;
+        int p=0,q=0;
+        int n=nums.size();
+        while(r<n)
+        {
+            if(m.find(nums[r])!=m.end())
+                l=max(l,m[nums[r]]+1);
+            m[nums[r]]=r;
+        
+            if(len<(r-l+1))
+            {
+               len=(r-l+1);
+                p=l;
+                q=r;
+            }  
+            
+            r++;
+        }
+    for(int i=p;i<=q;i++)
+    sum+=nums[i];
+    return sum; */
+        int maxSum = 0, currSum = 0;
+		unordered_map<int,int> seen; 
+        int i = 0,j = 0; // these are the bounds of current window
+		
+        while(j < nums.size())
+        {
+            if(!seen[nums[j]]) // if current element is not present in window, then add it to sum and mark it as seen
+            {
+                currSum += nums[j]; 
+                seen[nums[j]]++; 
+                j++; 
+                maxSum = max(maxSum, currSum);
+            }
+            else // if current element is already present in window, start trimming the window from the left
+            {
+                currSum -= nums[i]; 
+                seen[nums[i]]--;
+                i++;
+            }
+        }
+        return maxSum;
+    
+    }
+};
