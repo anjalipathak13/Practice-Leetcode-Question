8cf99dca83db616c1e6eb63b0a66e42f9d8a8056
Anjali Pathak <54896331+anjalipathak13@users.noreply.github.com>
2023-04-27 22:27:01 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (9.91%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        int c=0;
+        for(int i=1;i*i<=n;i++){
+            if(i*i<=n)
+                c++;
+        }
+        return c;
+    }
+};
