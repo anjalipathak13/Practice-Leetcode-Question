0aa3ec78d0c12db3b9839a4dc5138b22e6da0410
Anjali Pathak <54896331+anjalipathak13@users.noreply.github.com>
2023-07-16 08:49:46 +0530

    Added solution - LeetHub

---
+++ Queue Reversal - GFG/queue-reversal.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {   int n=q.size();
+        vector<int>v(n);
+        int i=n-1;
+        while(!q.empty()){
+            int temp=q.front();
+            q.pop();
+            v[i--] = temp;
+        }
+        
+        for(int i=0;i<v.size();i++){
+            q.push(v[i]);
+        }
+        return q;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
