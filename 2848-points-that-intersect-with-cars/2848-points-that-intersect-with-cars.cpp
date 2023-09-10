de1b15b76ff1cd7a1fd1f4e4d249be3d5d2ef896
Anjali Pathak <54896331+anjalipathak13@users.noreply.github.com>
2023-09-10 13:55:59 +0530

    Time: 20 ms (50.00%), Space: 25 MB (50.00%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.cpp
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int numberOfPoints(vector<vector<int>>& nums) {
-        int mini =INT_MAX;
         int maxi =INT_MIN;
-         
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums[0].size();j++){
-            mini = min(mini,nums[i][j]);
-            maxi = max(maxi,nums[i][j]);
-            }
-        }
        vector<int>vis(101,0);
-       for(int i=0;i<nums.size();i++){            
-               int l=nums[i][0];
-               int h=nums[i][1];
+       for(int i=0;i<nums.size();i++){  
+           int l=nums[i][0];
+           int h=nums[i][1];
+           maxi = max(maxi,l);
+           maxi = max(maxi,h);
                for(int k=l;k<=h;k++){
                    vis[k]=1;
                }
         }
         
-        int c=0;
+       int c=0;
        for(int i=1;i<=maxi;i++){
           if(vis[i])
                c++;
